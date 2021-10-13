7114e4d035e7994798c873dd2c07d6370f47fc96
smhrd <smhrd@DESKTOP-LCS063E>
2021-10-13 17:41:22 +0900

    Merge branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame4.git

--- github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame4.git/CalculatorGame4/src/MinusGame.java
+++ github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame4.git/CalculatorGame4/src/MinusGame.java
@@ -1,5 +1,29 @@
+import java.util.Random;
 
-public class MinusGame {
+public class MinusGame implements iGame{
+	
+	private Random rd;
+	private int num1;
+	private int num2;
+	
+	@Override
+	public void makeRandom() {
+		rd = new Random();
+		
+		num1 = rd.nextInt(9)+1;
+		num2 = rd.nextInt(9)+1;
+	}
+
+	@Override
+	public String getQuizMsg() {
+		return num1+" - "+num2+" = ";
+	}
+
+	@Override
+	public boolean checkAnswer(int answer) {
+		
+		return (answer == (num1-num2)) ? true : false;
+	}
 	
 	
 
