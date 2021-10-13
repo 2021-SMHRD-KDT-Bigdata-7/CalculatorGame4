de3993d4a3211b3d3bc5533a50a6e4f1e4abb74d
skalswn <92147848+skalswn@users.noreply.github.com>
2021-10-13 18:01:53 +0900

    마지막 나누기

---
+++ github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame4.git/CalculatorGame4/src/divideGame.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class divideGame implements iGame{
+	
+	private Random ran;
+	private int num1;
+	private int num2;
+
+	@Override
+	public void makeRandom() {
+		ran = new Random();
+		num1 = ran.nextInt(9)+1;
+		num2 = ran.nextInt(9)+1;
+		while(true) {
+			if(num1>num2&&num1%num2==0) {
+				break;
+			}else {
+				num1 = ran.nextInt(9)+1;
+				num2 = ran.nextInt(9)+1;
+			}
+		}
+	}
+
+	@Override
+	public String getQuizMsg() {
+		
+		return num1 + "/" + num2 + "=";
+	}
+
+	@Override
+	public boolean checkAnswer(int answer) {
+		
+		return (answer == (num1 / num2))? true : false;
+	}
+
+}
