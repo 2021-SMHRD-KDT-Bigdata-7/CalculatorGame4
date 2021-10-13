ee1f9b7affe785f388f2c01317edc1fb01ea75f0
skalswn <92147848+skalswn@users.noreply.github.com>
2021-10-13 18:08:02 +0900

    뭔가 바꿨는데 왜 커밋 푸쉬가 안 되는?

--- github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame4.git/CalculatorGame4/src/MinusGame.java
+++ github.com/2021-SMHRD-KDT-Bigdata-7/CalculatorGame4.git/CalculatorGame4/src/MinusGame.java
@@ -5,13 +5,19 @@ public class MinusGame implements iGame{
 	private Random rd;
 	private int num1;
 	private int num2;
+	private int num3;
 	
 	@Override
 	public void makeRandom() {
 		rd = new Random();
-		
 		num1 = rd.nextInt(9)+1;
 		num2 = rd.nextInt(9)+1;
+		num3 = 0;
+		if (num1<num2) {
+			num3=num1;
+			num1=num2;
+			num2=num3;
+		}
 	}
 
 	@Override
